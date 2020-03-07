c2efb0558a5e29ae9a7e6a9aa40c50d2732132b6
Osaou <osaourell@gmail.com>
2020-03-07 14:07:51 +0100

    first working country picker

--- country-picker/src/App.svelte
+++ country-picker/src/App.svelte
@@ -1,12 +1,39 @@
 <script>
+	import populations from './populations'
 
+	function findCountry(population) {
+		if (population == "") {
+			return ""
+		}
+
+		population = +population
+
+		return populations
+			.map(country => {
+				return {
+					distance: Math.abs(population - country["2018"]),
+					countryName: country["Country Name"]
+				}
+			})
+			.sort((a, b) => a.distance - b.distance)
+			[0].countryName
+	}
+
+	let typedValue = ""
+	$: chosenCountry = findCountry(typedValue)
 </script>
+
+
 
 <main>
 	<h1>Country Picker!</h1>
 
 	<label>
-		Estimate your desired country's population
-		<input type="number" />
+		Select country by population count
+		<input type="text" bind:value={typedValue} />
 	</label>
+
+	{#if chosenCountry != ""}
+		<p>You have chosen {chosenCountry}</p>
+	{/if}
 </main>
